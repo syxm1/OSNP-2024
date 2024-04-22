781cac2a071e1d02e7e5ad5f3d4fd536357587f0
Hisyam Prasetyo <106479331+syxm1@users.noreply.github.com>
2024-04-22 20:08:28 +0700

    Update SPOJ-NSITACMA.cpp

--- Gym/day-1/SPOJ-NSITACMA.cpp
+++ Gym/day-1/SPOJ-NSITACMA.cpp
@@ -28,3 +28,8 @@ signed main() {
 		tc();
 	}	
 }
+
+/*
+dari observasi, diperoleh bahwa suatu bilangan basis-n 'x' merupakan amusing digit apabila 'x' habis membagi n-1;
+sehingga banyak 'amusing-digit' pada sistem bilangan basis-n adalah banyak bilangan yang habis membagi x - 1 (meng-exclude '1').
+*/
