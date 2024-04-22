72c771f434cf3c4f1e2b9ff5b2721fb1ef431e87
Hisyam Prasetyo <106479331+syxm1@users.noreply.github.com>
2024-04-22 20:19:00 +0700

    Update SPOJ-SUMOFPOW.cpp

--- Gym/day-1/SPOJ-SUMOFPOW.cpp
+++ Gym/day-1/SPOJ-SUMOFPOW.cpp
@@ -30,3 +30,14 @@ signed main() {
 		test_case();
 	}
 }
+
+/*
+definisikan ans[n] := (p^n) + (q^n), x = p+q, y = pq.
+(p^(n-1) + q^(n-1)) * (p + q) = (p^n) + (q^n) + p^(n-1) * q + p * q^(n-1)
+				(p^n) + (q^n) = (p^(n-1) + q^(n-1)) * (p + q) - (p^(n-1) * q + p * q^(n-1))
+                (p^n) + (q^n) = (p^(n-1) + q^(n-1)) * (p + q) - (p^(n-2) + q^(n-2)) * pq
+sehingga diperoleh recurrence relation : ans[n] = ans[n-1] * x - ans[n-2] * y,
+dengan base case:
+ans[0] = p^0 + q^0 = 1 + 1 = 2
+ans[1] = p + q = x
+*/
