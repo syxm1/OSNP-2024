4786fa22231ba73422f970193ecbef9085581051
Hisyam Prasetyo <106479331+syxm1@users.noreply.github.com>
2024-04-24 19:43:57 +0700

    Update SPOJ-CSPATH.cpp

--- Gym/day-1/SPOJ-CSPATH.cpp
+++ Gym/day-1/SPOJ-CSPATH.cpp
@@ -1,14 +1,7 @@
 #include<iostream>
-using namespace std;
-#define l long long
-#define w while
-#define v (f!=i||g!=k)
-l c(l n,l r){l k=1,y=n-r;w(n)k*=n--;w(r)k/=r--;w(y)k/=y--;return k;}int main(){l t;cin>>t;w(t--){l x,y,f,g,i,k;cin>>x>>y>>f>>g>>i>>k;l u=c(x+y,y),a=c(x-f+y-g,x-f),b=c(f+g,f),d=c(x-i+y-k,x-i),e=c(i+k,i),p=f-i,q=g-k;if(p<0)p*=-1;if(q<0)q*=-1;l j=u-a*b,h=c(p+q,p);if(v)j-=d*e;if(f<=i&&g<=k&&v)j+=h*b*d;if(i<=f&&k<=g&&v)j+=h*a*e;cout<<j<<'\n';}}
-
+using l=int; l main() {l t;std::cin>>t;while(t--){l x,y,a,b,c,d;std::cin>>x>>y>>a>>b>>c>>d;l f[x+2][y+2];for(l i=0;i<=x+1;i++)f[i][0]=0;for(l i=0;i<=y+1;i++)f[0][i]=0;for(l i=1;i<=x+1;i++)for(l j=1;j<=y+1;j++){if(i==1&&j==1)f[i][j]=1;else if((i==a+1&&j==b+1)||(i==c+1&&j==d+1))f[i][j]=0;else f[i][j]=f[i-1][j]+f[i][j-1];}std::cout<<f[x+1][y+1]<<'\n';}}
 /*
-banyaknya jalan dari O(0,0) menuju titik A(x,y) tanpa melalui titik B(x1,y1) dan C(x2,y2)?
-x = banyak jalan (O-A) - banyak jalan(O-B-A) - banyak jalan(O-C-A), jika B != C.
-    banyak jalan (O-A) - banyak jalan(O-B-A), jika B == C.
-jika salah satu titik berada di kanan atas titik lain dan kedua titik tidak sama, 
-misal B berada di kanan atas C dan B != C, maka x += banyak jalan(O-C) * banyak jalan (C-B) * banyak jalan (A-B). 
+dp on grid untuk menghitung banyaknya jalan menuju (x,y).
+dp[i][j] = dp[i-1][j] + dp[i][j-1], titik (i,j) bukan (x1,y1) atau (x2,y2).
+           0, titik (i,j) merupakan salah satu dari (x1,y1) atau (x2,y2).
 */
