2f23146f6ae82cd666a1100e820e60cdb9a0e233
Hisyam Prasetyo <106479331+syxm1@users.noreply.github.com>
2024-04-26 14:32:09 +0700

    Create beecrowd-3472.cpp

---
+++ Gym/day-4/beecrowd-3472.cpp
@@ -0,0 +1,30 @@
+// Up there the piano
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool eliminated[5000];
+
+signed main() {
+	int n;
+	cin >> n;
+
+	const int J = 29, SZ = 2 * n;
+	int cur = 0;
+
+	n *= 2;
+
+	while (n > 1) {
+		int m = J%n-1;
+		if (m < 0) m += n;
+		while (m>0) {
+			if (!eliminated[cur]) m--;
+			cur = (cur + 1) % SZ;
+		}
+		while(eliminated[cur]) cur = (cur + 1) % SZ;
+		eliminated[cur++] = 1;
+		n--;
+	}
+
+	cout << (!eliminated[0] || !eliminated[1] ? "vai ganhar\n" : "nao vai ganhar\n");
+}
